54c3a0b3ee4a3c0b97336e2916377bbd3e6db8b6
Anastasiia Kuznetsova <nansyivanova@gmail.com>
2026-04-28 21:41:29 +0400

    добавила pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteImagemin from 'vite-plugin-imagemin';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Rick-and-Morty/',
   plugins: [
     {
       name: 'trailing-slash-redirect',
@@ -16,7 +16,8 @@ export default defineConfig({
             req.url.startsWith('/Rick-and-Morty') &&
             !req.url.startsWith('/Rick-and-Morty/')
           ) {
-            req.url = '/Rick-and-Morty/' + req.url.slice('/Rick-and-Morty'.length);
+            req.url =
+              '/Rick-and-Morty/' + req.url.slice('/Rick-and-Morty'.length);
           }
           next();
         });
@@ -28,8 +29,75 @@ export default defineConfig({
       pngquant: {
         quality: [0.7, 0.9]
       }
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      workbox: {
+        globPatterns: ['**/*.{html,css,js,png,svg}'], // <- особенно обратить внимание
+        globIgnores: ['**/screenshots/**'],
+        inlineWorkboxRuntime: true,
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/rickandmortyapi\.com\/.*$/, // <- особенно обратить внимание
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache'
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: 'Rick&Morty PWA App',
+        short_name: 'R&M_PWA',
+        description:
+          'PWA приложение со списком персонажей вселенной Рика и Морти',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/Rick-and-Morty/',
+        scope: '/Rick-and-Morty/',
+        orientation: 'portrait',
+        lang: 'ru-RU',
+        screenshots: [
+          // <- особенно обратить внимание
+          {
+            src: 'screenshots/desktop.png',
+            type: 'image/png',
+            sizes: '2107x1327',
+            form_factor: 'wide'
+          },
+          {
+            src: 'screenshots/mobile.png',
+            type: 'image/png',
+            sizes: '346x716',
+            form_factor: 'narrow'
+          }
+        ],
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'maskable-icon.png',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
     })
   ],
+  base: '/Rick-and-Morty/',
   resolve: {
     alias: {
       '@': '/src'
